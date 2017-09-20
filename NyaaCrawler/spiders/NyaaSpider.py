e41fa1e4c8f800201f63e5e8b130fd260af49832
vi000246 <vi000246@hotmail.com>
2017-09-20 14:55:36 +0800

    完成兩個nyaa站的爬取

--- NyaaCrawler/spiders/NyaaSpider.py
+++ NyaaCrawler/spiders/NyaaSpider.py
@@ -7,10 +7,8 @@ import scrapy
 from scrapy.http import Request
 from scrapy.cmdline import execute
 import re
-import json
-import random
 import sys
-import pandas as pd
+from dateutil import parser
 
 
 
@@ -48,21 +46,19 @@ class NyaaSpider(scrapy.Spider):
         self.maxage = kwargs.get('maxage','')
         self.minSize = kwargs.get('minSize','')
         self.maxSize = kwargs.get('maxSize','')
-
+        self.NyaadevUrl = 'https://sukebei.nyaa.si/'
+        self.NyaacatUrl = 'https://sukebei.pantsu.cat/feed/p/%s'
 
     def start_requests(self):
-        NyaadevUrl = 'https://sukebei.nyaa.si/?'
-        NyaaDevQuery = self.util.BuildUrl(NyaadevUrl,q=self.q, f='0', c='2_0') + '&p=%s'
+        NyaaDevQuery = self.util.BuildUrl(self.NyaadevUrl,q=self.q, f='0', c='2_0') + '&p=%s'
         # Rss位址 %s參數是頁數 寫死catalog
-        NyaacatUrl = 'https://sukebei.pantsu.cat/feed/p/%s?'
         NyaacatQuery = self.util.BuildUrl(
-                                        NyaacatUrl, sort=self.sort, fromDate=self.fromDate, toDate=self.toDate, maxage= self.maxage,
+                                        self.NyaacatUrl, sort=self.sort, fromDate=self.fromDate, toDate=self.toDate, maxage= self.maxage,
                                         order=self.sortOrder, minSize=self.minSize, maxSize=self.maxSize, c='2', q=self.q)
         # 取得指定頁數的頁面
         for page in range(1, self.page):
             yield scrapy.Request(url=NyaaDevQuery % page, callback=self.parseNyaadev)
-            # 組合出NyaaCat的Query Url
-            # yield scrapy.Request(url=NyaacatQuery % page, callback=self.parseNyaacat)
+            yield scrapy.Request(url=NyaacatQuery % page, callback=self.parseNyaacat)
 
     # search NyaaDev
     # p.s. 此站不給RSS filter 直接爬page
@@ -77,30 +73,42 @@ class NyaaSpider(scrapy.Spider):
         # # 將html存至html檔
         # with open(filename, 'wb') as f:
         #     f.write(response.body)
+
         items = response.xpath('//table//tr')
         for i in items:
             try:
                 videoInfo = ArticleList()
                 videoInfo["title"] = i.xpath('td[2]/a/text()').extract()[0]  # 影片標題
-                videoInfo["articlelink"] = items[1].xpath('td[2]/a/@href').extract()[0]  # 文章連結
-                videoInfo["torrent"] = items[1].xpath('td[3]/a[1]/@href').extract()[0]  # 種子連結 有torrent跟磁力 只取torrent
+                videoInfo["articlelink"] = self.NyaadevUrl + i.xpath('td[2]/a/@href').extract()[0]  # 文章連結
+                videoInfo["torrent"] = self.NyaadevUrl + i.xpath('td[3]/a[1]/@href').extract()[0]  # 種子連結 有torrent跟磁力 只取torrent
                 videoInfo["size"] = i.xpath('td[4]/text()').extract()[0]  # size
-                videoInfo["pubDate"] = i.xpath('td[5]/text()').extract()[0]  # date
+                videoInfo["pubDate"] = self.util.strToDateTime(i.xpath('td[5]/text()').extract()[0])  # date
                 videoInfo["seeder"] = i.xpath('td[6]/text()').extract()[0]  # seeder
                 videoInfo["leecher"] = i.xpath('td[7]/text()').extract()[0]  # leecher
                 videoInfo["downloads"] = i.xpath('td[8]/text()').extract()[0]  # downloads
-                yield videoInfo
+
+                # 如果有match到文章網址 就去爬文章的預覽圖
+                if (re.search('/view/\d+', videoInfo["articlelink"])):
+                    yield Request(videoInfo["articlelink"], callback=self.NyaaDevDetail, meta={'videoInfo': videoInfo})
             except IndexError:
                 pass
             continue
 
+    def NyaaDevDetail(self, response):
+        videoInfo = response.meta['videoInfo']
+        # 取出文章頁面的description區塊
+        item = response.xpath('//*[@id="torrent-description"]').extract()[0]
+        # 取出description區塊的url連結
+        urls = self.util.GetUrlFromHtml(item)
+        # 將預覽圖存檔
+        videoInfo["ImagePath"] = urls
+        yield videoInfo
 
 
 
 
 
-
-            # search NyaaCat by RSS feed
+    # search NyaaCat by RSS feed
     def parseNyaacat(self,response):
         # 匹配目前頁數
         # m = re.search("p/(\d+)", response.url)
@@ -115,19 +123,16 @@ class NyaaSpider(scrapy.Spider):
 
         # 取得RSS每個影片的物件
         items = response.xpath('//channel/item')
-        # match所有url
-        pattern = '(?P<url>(?:\w+):\/\/(?P<domain>[\w@][\w.:@-]+)\/?[\w\.?=%&=\-@/$,]*)'
+
         # 取出影片資訊
         for i in items:
             videoInfo = ArticleList()
             videoInfo["title"] = i.xpath('title/text()').extract()[0] # 文章標題
             videoInfo["torrent"] = i.xpath('link/text()').extract()[0] # 種子連結
-            regex = re.compile(pattern)
-            # match description裡所有的url 將url跟domain存在dict裡 格式 {'url':'xxx','url':'http://xxx/'}
-            urls = [m.groupdict() for m in regex.finditer(i.xpath('description/text()').extract()[0])]
-            videoInfo["ImagePath"] = urls # 取出預覽圖位址
+            urls = self.util.GetUrlFromHtml(i.xpath('description/text()').extract()[0])
+            videoInfo["ImagePath"] = urls  # 取出預覽圖位址
             videoInfo["articlelink"] = i.xpath('guid/text()').extract()[0] # 文章連結
-            videoInfo["pubDate"] = i.xpath('pubDate/text()').extract()[0] # 發佈日期
+            videoInfo["pubDate"] = self.util.strToDateTime(i.xpath('pubDate/text()').extract()[0]) # 發佈日期
             videoInfo["size"] = i.xpath('enclosure/@length').extract()[0] # 檔案大小
             yield videoInfo
 
--- NyaaCrawler/spiders/Utility.py
+++ NyaaCrawler/spiders/Utility.py
@@ -1,16 +1,23 @@
 from urllib.parse import urlencode
-import feedparser
+import re
+from dateutil import parser
 
 class Utility:
     # 依據搜尋關鍵字 組合出搜尋url
     @staticmethod
     def BuildUrl(url, **querys):
-        return url + urlencode(querys)
+        return url + '?' + urlencode(querys)
 
-    # 匹配html 找出文章
+    # 輸入html 找出所有的網址連結 回傳dict {url:'xxx','domain':'xxx'}
+    def GetUrlFromHtml(self,html):
+        # match所有url
+        pattern = '(?P<url>(?:\w+):\/\/(?P<domain>[\w@][\w.:@-]+)\/?[\w\.?=%&=\-@/$,]*)'
+        regex = re.compile(pattern)
+        # match description裡所有的url 將url跟domain存在dict裡 格式 {'url':'xxx','url':'http://xxx/'}
+        urls = [m.groupdict() for m in regex.finditer(html)]
+        return urls
 
-    def GetNyaaDevItem(self):
-        pass
-
-    def GetNyaaCatItem(self):
-        pass
+    # 將字串轉為YYYY-MM-DD HH:MM:SS的DateTime格式
+    def strToDateTime(self,text):
+        dt = parser.parse(text)
+        return dt.strftime("%Y-%m-%d %H:%M:%S")
