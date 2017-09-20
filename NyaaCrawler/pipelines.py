129dd38c75dd76dcc130f1afdc9fd91546838b47
vi000246 <vi000246@hotmail.com>
2017-09-20 17:02:35 +0800

    將DB連線從config檔撈取

--- NyaaCrawler/pipelines.py
+++ NyaaCrawler/pipelines.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import pymongo
 from .items import ArticleList
+from scrapy.conf import settings
 class NyaacrawlerPipeline(object):
     def __init__(self):
-        clinet = pymongo.MongoClient("localhost", 27017)
-        db = clinet["Nyaa"]
-        self.dataset = db["Article"]
+        clinet = pymongo.MongoClient(settings['MONGODB_HOST'], settings['MONGODB_PORT'])
+        db = clinet[settings['MONGODB_DB']]
+        self.dataset = db[settings['MONGODB_COLLECTION']]
+
         # 建立索引
         self.dataset.create_index([('title', pymongo.ASCENDING)] ,unique=True)
+
 
     def process_item(self, item, spider):
         print
