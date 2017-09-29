794b95b8066898bde9b8bf35a8483d6299e2b309
vi000246 <vi000246@hotmail.com>
2017-09-29 16:29:48 +0800

    完成imgrock

--- NyaaCrawler/flask_web/ImageParse/ImageHandler.py
+++ NyaaCrawler/flask_web/ImageParse/ImageHandler.py
@@ -50,11 +50,13 @@ class Image:
             "xxxwebdlxxx.org": "imgyt",
             "imgtornado.com": "imgyt",
             # ===================================
+            # 需要取得轉址後連結 並夾帶hidden參數
+            "imgrock.co": "imgrock",
+            "r01.imgrock.co": "imgrock",
+
 
             "io1.imgoutlet.co": "NotSupport", # 這圖床的continue按鈕好像是用js載入的 待研究
             "imgoutlet.co": "NotSupport", # 原理同上  continue button是用js載入的
-            "imgrock.co": "NotSupport", # 同上 continue button是用js載入的
-            "r01.imgrock.co": "NotSupport",# 同上 continue button是用js載入的
 
              "www.imgbabes.com":"NotSupport", # 會用到javascript 先跳過
             "www.imgflare.com": "NotSupport",# 同上 會用到js
---
+++ NyaaCrawler/flask_web/ImageParse/imgrock.py
@@ -0,0 +1,52 @@
+import requests
+import re
+from lxml import etree
+
+class imgrock(object):
+    def __init__(self, url):
+        self.url = url
+
+    def get(self):
+        BigImgUrl = ''
+        # 取得fileCode 用來加在cookie 用在step2的Get request
+        m = re.match(r"http://[\w\.]+\/(?P<fileCode>\w+)\/.*html", self.url)
+        fileCode = ''
+        if m:
+            fileCode = m["fileCode"]
+
+        # 如果是連結網址就進行request 縮圖網址就忽略
+        if re.match(r"http.*(?=html)", self.url):
+            s = requests.session()
+            r = s.get(self.url)
+            # 抓取php連結 此為真正的連結
+            realLink = re.search(r"http://(\w|\.)+\/.*\.php",r.text)[0]
+
+            r2 = s.get(realLink,data={'fileCode':fileCode})
+            # 因為隱藏欄位是用js產生的 所以直接match javascript裡的<input type="hidden"的值>
+            # hiddenValue = re.search(r"<input\\x20type=\\x22hidden\\x22\\x20name=\\x22\'\+\'(?P<hiddenValue>[^']+)",r2.text)
+
+            # 更新 不是match <input type="hidden"的值 而是match類似下面的這串值
+            # (_0x7995('0x72','TB3R'),"a3fef360d8a2279ab2d067f4554280e4")
+            hiddenValue = re.search(r"\(_0x7995\(\'0x72\',\'TB3R\'\)\,\"(?P<hiddenValue>[^\"]+)",r2.text)
+
+            # 再向php連結post一次
+            headers = {'Content-type': 'application/x-www-form-urlencoded'}
+            data = {"op":"view","id":fileCode,"pre":"1",hiddenValue["hiddenValue"]:"1"}
+
+            r3 = s.post(realLink,headers=headers,data=data)
+            BigImgUrl = re.search(r"img\ssrc=\"(?P<img>http://[^\/]+\/img\/[^\"]+\.jpe?g)\"",r3.text)["img"]
+
+        else:
+            BigImgUrl = self.url
+
+
+
+
+        return BigImgUrl
+
+if __name__ == '__main__':
+    imh = imgrock("http://imgrock.co/15lok1ijuoj0/MVSD-009.jpg.html")
+    # imh = imgrock("http://r01.imgrock.co/i/00269/sq3ja45cjoqv_t.jpg")
+    # imh = imgrock("http://imgoutlet.co/3rf9nnv9sv6u /MUM-017.jpg.html")
+
+    print(imh.get())
